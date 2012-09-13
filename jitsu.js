82d4f6a66ed0b3759999407aae47f710e8964032
c4s4 <michel.casabianca@gmail.com>
2012-09-13 10:49:00 +0200

    first commit

---
+++ jitsu.js
@@ -0,0 +1,147 @@
+importPackage(javax.script.argv);
+
+////////////////////////////////////////////////////////////////////////////////
+//                        UTILITY FUNCTIONS FOR TESTS                         //
+////////////////////////////////////////////////////////////////////////////////
+
+function assertEqual(expected, actual, message) {
+    if(expected != actual) {
+        if(message) throw message;
+        else throw "'" + expected + "' expected to be equal to '" + actual + "'";
+    }
+}
+
+function assertNotEqual(expected, actual, message) {
+    if(expected == actual) {
+        if(message) throw message;
+        else throw "'" + expected + "' expected not to be equal to '" + actual + "'";
+    }
+}
+
+function assertStrictEqual(expected, actual, message) {
+    if(expected !== actual) {
+        if(message) throw message;
+        else throw "'" + expected + "' expected to be strictly equal to '" + actual + "'";
+    }
+}
+
+function assertStrictNotEqual(expected, actual, message) {
+    if(expected === actual) {
+        if(message) throw message;
+        else throw "'" + expected + "' expected not to be strictly equal to '" + actual + "'";
+    }
+}
+
+function assertTrue(actual, message) {
+    if(!actual) {
+        if(message) throw message;
+        else throw "'" + actual + "' was supposed to be true";
+    }
+}
+
+function assertFalse(actual, message) {
+    if(actual) {
+        if(message) throw message;
+        else throw "'" + actual + "' was supposed to be false";
+    }
+}
+
+function assertRaise(testFunction, pattern, message) {
+    var error = false;
+    var errorMessage = null;
+    try {
+        testFunction();
+    } catch(exception) {
+        error = true;
+        errorMessage = String(exception);
+    }
+    if(message && (!error || !errorMessage.match(pattern))) throw message;
+    else {
+        if(!error) throw "No exception was raised"
+        if(error && !errorMessage.match(pattern)) throw "Error message doesn't match pattern"
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//                             UNIT TEST RUNNER                               //
+////////////////////////////////////////////////////////////////////////////////
+
+var errors = {}
+var error = false
+
+function getNames() {
+    var names = []
+    for (var name in this) {
+        names.push(name);
+    }
+    return names;
+}
+
+function arrayContains(aray, element) {
+    for(var i=0; i<aray.length; i++) {
+        if(aray[i] == element) return true;
+    }
+    return false;
+}
+
+function runTestFile(testFileName) {
+    testOK = true;
+    print("Running '" + testFileName + "'");
+    out.flush();
+    var namesBefore = getNames();
+    load(testFileName);
+    var namesAfter = getNames();
+    var functions = [];
+    for(index in namesAfter) {
+        var name = namesAfter[index];
+        if(!arrayContains(namesBefore, name)) {
+            if(typeof(this[name]) == "function" && name.match(/^test/)) {
+                functions.push(name);
+            }
+        }
+    }
+    for(var index in functions) {
+        print(".");
+        out.flush();
+        if(this["setUp"]) setUp();
+        try {
+            this[functions[index]]();
+        } catch(exception) {
+            if(!errors[testFileName]) {
+                errors[testFileName] = {}
+            }
+            errors[testFileName][functions[index]] = String(exception);
+            error = true;
+            testOK = false;
+        }
+        if(this["tearDown"]) tearDown();
+    }
+    if(testOK) println(" OK");
+    else println(" ERROR");
+    // delete all that was defined loading and running test file
+    var namesAfter = getNames();
+    for(index in namesAfter) {
+        var name = namesAfter[index];
+        if(!arrayContains(namesBefore, name)) {
+            delete this[name];
+        }
+    }
+}
+
+println("Loading " + arguments.length + " JavaScript test file(s)");
+for(var i=0; i<arguments.length; i++) {
+    runTestFile(String(arguments[i]));
+}
+
+if(error) {
+    println("ERRORS:");
+    for(var testFileName in errors) {
+        println(testFileName + ":");
+        for(var functionName in errors[testFileName]) {
+            println("- " + functionName + ": " + errors[testFileName][functionName]);
+        }
+    }
+    exit(1);
+} else {
+    println("OK");
+}
---
+++ testJitsu.js
@@ -0,0 +1,80 @@
+function setUp() {
+    setUpRun = true;
+}
+
+function tearDown() {
+    tearDownRun = true;
+}
+
+function testSetUp() {
+    assertTrue(typeof(setUpRun) == "boolean" && setUpRun);
+}
+
+function testTearDown1() {
+    if(typeof(testTearDown) == "boolean" && testTearDown) {
+        assertTrue(tearDownRun)
+    }
+    testTearDown = true;
+}
+
+function testTearDown2() {
+    if(typeof(testTearDown) == "boolean" && testTearDown) {
+        assertTrue(tearDownRun)
+    }
+    testTearDown = true;
+}
+
+function testAssertEqual() {
+    assertEqual(1, 1);
+    assertEqual(1, "1");
+    assertEqual("1", 1);
+}
+
+function testAssertNotEqual() {
+    assertNotEqual(1, 2);
+    assertNotEqual(1, "2");
+    assertNotEqual("2", 1);
+}
+
+function testAssertStrictEqual() {
+    assertStrictEqual(1, 1);
+}
+
+function testAssertStrictNotEqual() {
+    assertStrictNotEqual(1, 2);
+    assertStrictNotEqual(1, "1");
+}
+
+function testAssertTrue() {
+    assertTrue(true);
+    assertTrue(1);
+    assertTrue("abc");
+    assertTrue([1, 2, 3]);
+}
+
+function testAssertFalse() {
+    assertFalse(false);
+    assertFalse(0);
+    assertFalse(null);
+}
+
+function testAssertRaise() {
+    assertRaise(function() {throw "test";}, /test/);
+    assertRaise(function() {throw "test";}, /test/, "test");
+    assertRaise(function() {assertRaise(function() {var i = 0;}, /./);}, /^No exception was raised$/);
+    assertRaise(function() {assertRaise(function() {throw "test";}, /toto/);}, /^Error message doesn't match pattern$/);
+    assertRaise(function() {assertRaise(function() {var i = 0;}, /./, "No error");}, /^No error$/);
+}
+
+function testSleep() {
+    sleep(1000);
+}
+
+function sleep(time) {
+    d = new Date();
+    diff = 0;
+    while(diff < time) {
+        n = new Date();
+        diff = n - d;
+    }
+}
